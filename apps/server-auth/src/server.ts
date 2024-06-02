3daa36904cd5a04a4e1466f61d5b4ace90851fe0
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-06-02 22:49:37 +0800

    init @auth/express

--- apps/server-auth/src/server.ts
+++ apps/server-auth/src/server.ts
@@ -3,11 +3,15 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 
+import { ExpressAuth } from '@auth/express'
+import Google from '@auth/express/providers/google'
+
 export function createExpressServer(): Express {
   const app = express()
 
-  app.set('port', 8080)
   app.disable('x-powered-by')
+
+  app.set('port', 8080).set('trust proxy', true)
 
   app
     .use(helmet())
@@ -15,6 +19,18 @@ export function createExpressServer(): Express {
     .use(express.json())
     .use(cors({ credentials: true }))
     .use(express.urlencoded({ extended: true }))
+
+  app.use(
+    '/api/auth/*',
+    ExpressAuth({
+      providers: [
+        Google({
+          clientId: '',
+          clientSecret: ''
+        })
+      ]
+    })
+  )
 
   return app
 }
