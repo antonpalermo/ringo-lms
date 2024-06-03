fbc0ab37dad6f18469f0722606f9647fc194e27f
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-06-03 20:45:41 +0800

    fix: next/babel errors

--- apps/web-client/postcss.config.js
+++ apps/web-client/postcss.config.js
@@ -1,8 +1,8 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+/**
+ * @type { import('postcss-load-config').Config }
+ **/
+module.exports = {
   plugins: {
     tailwindcss: {}
   }
 }
-
-export default config
