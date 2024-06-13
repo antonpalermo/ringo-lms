0b159646e69cf0db210b46205de0b7659fe21f74
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-06-13 22:49:15 +0800

    refac: move create groups

---
+++ apps/web-client/app/(create)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@/app/(create)/_components/navbar'
+
+interface CreateRootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function CreateRootLayout({ children }: CreateRootLayoutProps) {
+  return (
+    <>
+      <Navbar />
+      <main className='w-full my-5'>{children}</main>
+    </>
+  )
+}
