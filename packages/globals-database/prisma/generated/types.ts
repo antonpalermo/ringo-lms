a8d0bde78a241c1f7097e8a9eca2f9b4b45190cd
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-06-26 22:40:42 +0800

    added chapters schema

--- packages/globals-database/prisma/generated/types.ts
+++ packages/globals-database/prisma/generated/types.ts
@@ -1,10 +1,17 @@
 import type { ColumnType } from 'kysely'
-
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>
 
 export type Timestamp = ColumnType<Date, Date | string, Date | string>
+
+export type Chapter = {
+  id: Generated<string>
+  title: string
+  description: string
+  dateCreated: Generated<Timestamp>
+  dateUpdated: Generated<Timestamp>
+}
 
 export type Course = {
   id: Generated<string>
@@ -15,7 +22,6 @@ export type Course = {
 }
 
 export type DB = {
+  chapters: Chapter
   courses: Course
 }
-
-export * from 'kysely'
