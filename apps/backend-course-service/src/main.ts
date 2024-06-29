67807d6e5bf7af331c7d22d188ce5a92cbb5d4b0
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-06-30 00:22:09 +0800

    update course service module

--- apps/backend-course-service/src/main.ts
+++ apps/backend-course-service/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
-import { AppModule } from './app.module'
+import { CoursesModule } from './courses/courses.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(CoursesModule)
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
