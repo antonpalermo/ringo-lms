a4becd23e47fd71e6f37b7b68330123620ea01b7
Anton Angelo Palermo <anpbusinessmail@gmail.com>
2024-06-30 17:41:50 +0800

    refac: remove old codebase

--- apps/web-client/libs/errors.ts
+++
@@ -1,4 +0,0 @@
-export const Errors = {
-  INVALID_COURSE_DETAILS: 'Provided course details is not valid',
-  INTERNAL_SERVER_ERROR: 'Internal Server Error'
-}
--- apps/web-server/src/routes/status.ts
+++
@@ -1,9 +0,0 @@
-import express, { type Router } from 'express'
-
-const router: Router = express.Router()
-
-router.get('/healthz', (req, res) => {
-  return res.status(200).json({ status: 'healthy', ok: true })
-})
-
-export default router
