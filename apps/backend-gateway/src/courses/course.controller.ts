f8784cfe82d21c8ff89b3a70ab83eec829129e47
Anton Angelo Palermo <anpbusinessmail@gmail.com>
2024-07-02 17:49:24 +0800

    refac: update course schema validations

--- apps/backend-gateway/src/courses/course.controller.ts
+++ apps/backend-gateway/src/courses/course.controller.ts
@@ -5,39 +5,48 @@ import {
   Delete,
   Body,
   Param,
-  Controller
+  Controller,
+  ParseUUIDPipe,
+  UsePipes
 } from '@nestjs/common'
 
 import { CourseService } from './course.service'
 import { CreateCourseDTO } from './dto/create-course.dto'
 import { UpdateCourseDTO } from './dto/update-course.dto'
+import { ZodValidationPipe } from '../pipes/zod.pipe'
+
+import { courseSchema } from '../courses/dto/create-course.dto'
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
   @Get()
-  getCourses() {
+  getCourses(): any {
     return this.courseService.fetchAllCourse()
   }
 
   @Get(':id')
-  getCourse(@Param('id') id: string) {
+  getCourse(@Param('id', new ParseUUIDPipe()) id: string): any {
     return this.courseService.fetchCourse(id)
   }
 
   @Post('create')
-  createCourse(@Body() course: CreateCourseDTO) {
+  @UsePipes(new ZodValidationPipe(courseSchema))
+  createCourse(@Body() course: CreateCourseDTO): any {
     return this.courseService.createCourse(course)
   }
 
   @Patch(':id/update')
-  updateCourse(@Param('id') id: string, @Body() course: UpdateCourseDTO) {
+  updateCourse(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() course: UpdateCourseDTO
+  ): any {
     return this.courseService.updateCourse(id, course)
   }
 
   @Delete(':id')
-  deleteCourse(@Param('id') id: string) {
+  deleteCourse(@Param('id', new ParseUUIDPipe()) id: string): any {
     return this.courseService.deleteCourse(id)
   }
 }
--- apps/backend-gateway/src/courses/dto/create-course.dto.ts
+++ apps/backend-gateway/src/courses/dto/create-course.dto.ts
@@ -1,4 +1,10 @@
-export class CreateCourseDTO {
-  name: string
-  description: string
-}
+import { z } from 'zod'
+
+export const courseSchema = z
+  .object({
+    name: z.string(),
+    description: z.string()
+  })
+  .required()
+
+export type CreateCourseDTO = z.infer<typeof courseSchema>
