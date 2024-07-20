b1606d2be20c460410b76183145a6dc7e3a7105b
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-07-20 23:11:13 +0800

    refac: update dashboard layout

--- apps/web-app/src/pages/courses.tsx
+++ apps/web-app/src/pages/courses.tsx
@@ -1,6 +1,7 @@
 import useDocumentTitle from '@/hooks/use-document-title'
 import { useLoaderData } from 'react-router-dom'
 import { Course } from '@/lib/types'
+import { Card, CardContent } from '@/components/ui/card'
 
 export default function CoursesPage() {
   useDocumentTitle('Courses')
@@ -8,14 +9,22 @@ export default function CoursesPage() {
   const courses = useLoaderData() as Course[]
 
   return (
-    <div>
-      <h1>Courses</h1>
-      {courses.map(course => (
-        <div>
-          <h2>{course.name}</h2>
-          <p>{course.description}</p>
-        </div>
-      ))}
-    </div>
+    <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+      <div className='flex items-center'>
+        <h1 className='text-lg font-semibold md:text-2xl'>Courses</h1>
+      </div>
+      <div className='grid lg:grid-cols-3 md:grid-cols-1 gap-5'>
+        {courses.map(course => (
+          <Card>
+            <CardContent>
+              <h2 className='mt-6 text-lg font-medium'>{course.name}</h2>
+              <p className='text-muted-foreground text-sm'>
+                {course.description}
+              </p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </main>
   )
 }
--- apps/web-app/src/pages/layouts/dashboard.layout.tsx
+++ apps/web-app/src/pages/layouts/dashboard.layout.tsx
@@ -1,5 +1,29 @@
+import { Button } from '@/components/ui/button'
+import { Package2, Bell } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '@/components/dashboard/navbar'
 
 export default function DashboardLayout() {
-  return <Outlet />
+  return (
+    <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
+      <div className='hidden border-r bg-muted/40 md:block'>
+        <div className='flex h-full max-h-screen flex-col gap-2'>
+          <div className='flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6'>
+            <a href='/' className='flex items-center gap-2 font-semibold'>
+              <Package2 className='h-6 w-6' />
+              <span className=''>Ringo</span>
+            </a>
+            <Button variant='outline' size='icon' className='ml-auto h-8 w-8'>
+              <Bell className='h-4 w-4' />
+              <span className='sr-only'>Toggle notifications</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
+  )
 }
