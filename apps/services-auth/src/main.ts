7b5b78d35a316a38cc4096a307fcdce838c32b9e
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-07-25 21:21:28 +0800

    refac: update auth dir

--- apps/services-auth/src/main.ts
+++ apps/services-auth/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
-import { AuthModule } from './auth/auth.module'
+import { AuthModule } from './auth.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule)
